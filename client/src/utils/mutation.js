290e01ee80ac678eaa66690d6cfe804967efa72f
fasikaWalle <fasikabini12@gmail.com>
2021-05-15 15:25:12 -0500

    add save book mutation

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -22,3 +22,20 @@ export const ADD_USER = gql`
     }
   }
 `;
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: bookInput) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
